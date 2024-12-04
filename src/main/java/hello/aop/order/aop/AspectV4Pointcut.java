06d775cfd9c2f3ba3bd597369d6db3ce7f9247b2
carus-lee <leec8.2019@gmail.com>
2024-12-04 09:03:43 +0900

    스프링 AOP 구현 - 외부 포인트컷 참조  > @Around에 "패키지경로 + 클래스명 + 메서드명"으로 사용

---
+++ src/main/java/hello/aop/order/aop/AspectV4Pointcut.java
@@ -0,0 +1,39 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class AspectV4Pointcut {
+
+    //외부 포인트컷 --> "패키지경로 + 클래스명 + 메서드명"
+    @Around("hello.aop.order.aop.Pointcuts.allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature()); //join point 시그니처
+        return joinPoint.proceed();
+    }
+
+    //외부 포인트컷 --> "패키지경로 + 클래스명 + 메서드명"
+    //hello.aop.order 패키지와 하위패키지 이면서 클래스이름 패턴이 *Service
+    @Around("hello.aop.order.aop.Pointcuts.orderAndService()")
+    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        try {
+            log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
+            //커밋
+            Object result = joinPoint.proceed();
+            log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
+            return result;
+        } catch (Exception e) {
+            log.info("[트랜잭션 롤백] {}", joinPoint.getSignature());
+            throw e;
+        } finally {
+            log.info("[트랜잭션 릴리즈] {}", joinPoint.getSignature());
+        }
+    }
+
+}
