8ddfb5622400c941704e104a49c9e32076885c2e
carus-lee <leec8.2019@gmail.com>
2025-01-03 09:04:13 +0900

    스프링 AOP > 실전예제

---
+++ src/main/java/hello/aop/exam/ExamRepository.java
@@ -0,0 +1,25 @@
+package hello.aop.exam;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+public class ExamRepository {
+
+    private static int seq = 0;
+
+    /**
+     * 5번에 1번씩 에러 발생시킴
+     */
+    public String save(String itemId) {
+
+        seq++;
+        log.info("itemId={}, curr Seq={}", itemId, seq);
+        if (seq % 5 == 0) {
+            throw new IllegalStateException("예외 발생");
+        }
+        return "ok";
+    }
+
+}
