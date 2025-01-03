1ad3b910ac58bd960049edf1ef4b1a89d7dae061
carus-lee <leec8.2019@gmail.com>
2025-01-03 09:12:08 +0900

    스프링 AOP 실전 - 로그 출력 AOP > Aspect 적용 및 메서드에 적용할 annotation(Trace) 추가

--- src/main/java/hello/aop/exam/ExamRepository.java
+++ src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,5 +1,6 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Trace;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
@@ -12,10 +13,11 @@ public class ExamRepository {
     /**
      * 5번에 1번씩 에러 발생시킴
      */
+    @Trace
     public String save(String itemId) {
 
         seq++;
-        log.info("itemId={}, curr Seq={}", itemId, seq);
+        //log.info("itemId={}, curr Seq={}", itemId, seq);
         if (seq % 5 == 0) {
             throw new IllegalStateException("예외 발생");
         }
