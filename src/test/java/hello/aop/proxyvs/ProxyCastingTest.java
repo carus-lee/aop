0d8382693307a990490f18087fa8acbfa771d0c3
carus-lee <leec8.2019@gmail.com>
2025-01-24 08:05:25 +0900

    스프링 AOP 실무 주의사항 - 프록시 기술의 한계 > 의존관계 주입

---
+++ src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,44 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+@Slf4j
+public class ProxyCastingTest {
+
+    @Test
+    void jdkProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false); //JDK 동적 프록시
+
+        //프록시를 인터페이스로 캐스팅 성공 --> target의 인터페이스 기반으로 프록시를 생성
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        log.info("proxy class={}", memberServiceProxy.getClass());
+
+        //JDK 동적 프록시를 구현 클래스로 캐스팅 시도 실패, ClassCastException 예외 발생
+        Assertions.assertThrows(ClassCastException.class, () -> {
+            MemberServiceImpl memberServiceImplCasting = (MemberServiceImpl) proxyFactory.getProxy();
+        });
+    }
+
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true); //CGLIB 프록시
+
+        //프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        log.info("proxy class={}", memberServiceProxy.getClass());
+
+        //CGLIB 프록시를 구현 클래스로 캐스팅 시도 성공
+        MemberServiceImpl memberServiceImplCasting = (MemberServiceImpl) proxyFactory.getProxy();
+    }
+}
